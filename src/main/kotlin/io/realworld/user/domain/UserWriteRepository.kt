4a48d53fae1cfbb088a012bf2dd9c8049ad675e6
Robert Sawicki <rsw@touk.pl>
2019-01-22 10:01:24 +0100

    Add update user endpoint

--- src/main/kotlin/io/realworld/user/domain/UserWriteRepository.kt
+++ src/main/kotlin/io/realworld/user/domain/UserWriteRepository.kt
@@ -1,5 +1,7 @@
 package io.realworld.user.domain
 
 interface UserWriteRepository {
+    fun create(user: User): User
+
     fun save(user: User): User
 }
--- src/main/kotlin/io/realworld/user/endpoint/UserEndpoint.kt
+++ src/main/kotlin/io/realworld/user/endpoint/UserEndpoint.kt
@@ -1,13 +1,20 @@
 package io.realworld.user.endpoint
 
 import io.realworld.security.domain.LoggedUserService
+import io.realworld.user.domain.UserUpdateService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping(UserEndpoint.PATH)
-class UserEndpoint(private val loggedUserService: LoggedUserService) {
+class UserEndpoint(
+        private val loggedUserService: LoggedUserService,
+        private val userUpdateService: UserUpdateService
+) {
 
     companion object {
         const val PATH = "/user"
@@ -15,4 +22,9 @@ class UserEndpoint(private val loggedUserService: LoggedUserService) {
 
     @GetMapping
     fun current() = UserResponse(loggedUserService.loggedUser()?.toDto())
+
+    @PutMapping
+    fun update(@Valid @RequestBody user: UpdateDto) = UserResponse(
+            userUpdateService.update(loggedUserService.loggedUserOrThrow(), user).toDto()
+    )
 }
